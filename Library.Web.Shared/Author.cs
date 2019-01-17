892a36fbb71940b35e88dfd0a79387bc5cb4e25b
doBRoDuSHKo\BoyanMedarski <boyamedarski@mail.bg>
2019-01-17 14:57:07 +0200

    Added Author and Book properties

---
+++ Library.Web.Shared/Author.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Web.Shared
+{
+	public class Author
+	{
+		public int Id { get; set; }
+		public int GoodReadsId { get; set; }
+		public string Name { get; set; }
+		public string ImageUrl { get; set; }
+		public string SmallImageUrl { get; set; }
+		public string Link { get; set; }
+
+	}
+}
--- Library.Web.Shared/Book.cs
+++ Library.Web.Shared/Book.cs
@@ -1,7 +1,27 @@
 ﻿namespace Library.Web.Shared
 {
+	using System.Collections.Generic;
+
 	public class Book
 	{
-
+		public Book()
+		{
+			this.Authors = new HashSet<Author>();
+		}
+		public int Id { get; set; }
+		public int GoodReadsId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string ImageUrl { get; set; }
+		public string SmallImageUrl { get; set; }
+		public string Url { get; set; }
+		public int Isbn { get; set; }
+		public ICollection<Author> Authors { get; set; }
+		public string Language { get; set; }
+		public int Pages { get; set; }
+		public bool IsRead { get; set; }
+		public bool DoesHaveIt { get; set; }
+		public bool IsPublished { get; set; }
+		public bool InToDoList { get; set; }
 	}
 }
